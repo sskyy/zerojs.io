9bc68228d53a811a566a881ab199c40180893a68
青栀 <fanzheng.fz@alibaba-inc.com>
2014-11-18 22:04:41 +0800

    add blacklist

--- modules/official/zero-search.js
+++ modules/official/zero-search.js
@@ -3,14 +3,28 @@ module.exports = exports = search
 var request = require('request')
 var Q = require('Q')
 
-// TODO, need to add a blacklist
+var blackList = [
+  'zero-blog',
+  'zero-chat',
+  'zero-config',
+  'zero-crossings',
+  'zero-framework',
+  'zero-s3'
+]
+
 function search() {
   var defer = Q.defer();
   var url = 'https://registry.npmjs.org/-/all?startkey="zero-a"&endkey="zero-z"';
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-      defer.resolve(JSON.parse(body))
+      var data = JSON.parse(body);
+
+      blackList.forEach(function(packageName) {
+        delete data[packageName]
+      })
+
+      defer.resolve(data)
     } else {
       defer.reject(error)
     }
