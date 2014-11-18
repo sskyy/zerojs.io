fd669af2fa107fe084eb2abc2bb9af023b72c975
青栀 <fanzheng.fz@alibaba-inc.com>
2014-11-18 20:46:47 +0800

    rewrite modules page's ui

--- modules/official/public/scripts/modules.js
+++ modules/official/public/scripts/modules.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
        if(data) {
          delete data._updated
 
-         html = '<nav><ul>'
+         html = '<nav><ul class="row collapse">'
 
          for(x in data) {
            if(data.hasOwnProperty(x)) {
@@ -23,14 +23,16 @@ $(document).ready(function() {
 
              var author = data[x].author? data[x].author['name']: 'unknow';
 
-             html += '<li>'
+             html += '<li class="large-4 columns">'
              html += '<div class="package">'
-               html += '<h4>' + data[x].name + '</h4>'
-               html += '<p class="author-name">By '
-                html += '<span class="author">' + author + '</span>'
-                html += ' Latest version: <span class="version">' + version + '</span>'
-               html += '</p>'
-               html += '<div class="description">' + data[x].description
+               html += '<h5>' + data[x].name + '</h5>'
+               html += '<div class="content">'
+                 html += '<div class="description">' + data[x].description
+                 html += '</div>'
+                 html += '<div class="detail">Author: '
+                  html += '<span class="author">' + author + '</span>'
+                  html += '<span class="version-wrapper"> Latest version: <span class="version">' + version + '</span></span>'
+                 html += '</div>'
                html += '</div>'
              html += '</div>'
              html += '</li>'
